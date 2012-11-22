48a913052e77dc46a5d71e810ebe34f072e1a51d
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-11-22 16:33:02 +0000

    switch to IDEA 12 branch, remove -trunk suffix in task names
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk/TeamCity.Downloader@151 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

--- sheduled-task.bat
+++ sheduled-task.bat
@@ -6,7 +6,7 @@ echo Started fetch >> last-fatch-log.txt
 time /T >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
 
-cmd /c ant idea-pre-fetch-trunk 2>&1 | wtee -a last-fatch-log.txt
+cmd /c ant idea-pre-fetch 2>&1 | wtee -a last-fatch-log.txt
 
 echo. >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
