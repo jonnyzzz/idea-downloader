83d74c45f43f12bbc59e1ab75c733450d1cfda50
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-08-10 10:31:13 +0000

    make necessary CD to the idea folder automatically
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk/TeamCity.Downloader@174 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

--- run.bat
+++ run.bat
@@ -1,4 +1,21 @@
 set IDEA_JDK=C:\Java\jdk1.7.0_21
+
+IF EXIST ivy-idea-trunk.xml SET this_is_idea_update_home=jonnyzzz
+
+IF %this_is_idea_update_home% == "" GOTO under_idea_folder
+
+  cd ..
+  cd idea
+
+:under_idea_folder
+
 cd bin
 call idea.bat
 cd ..
+
+IF %this_is_idea_update_home% == "" GOTO under_idea_folder_done
+
+  cd ..
+  cd idea.update
+
+:under_idea_folder_done
