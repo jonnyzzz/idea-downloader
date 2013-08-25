88b55a97b4108bc8e8edc0d3b846bd3fbb3aad46
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-08-25 13:52:57 +0000

    fix start from IDEA folder
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk/TeamCity.Downloader@175 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ set IDEA_JDK=C:\Java\jdk1.7.0_21
 
 IF EXIST ivy-idea-trunk.xml SET this_is_idea_update_home=jonnyzzz
 
-IF %this_is_idea_update_home% == "" GOTO under_idea_folder
+IF "%this_is_idea_update_home%" == "" GOTO under_idea_folder
 
   cd ..
   cd idea
@@ -13,7 +13,7 @@ cd bin
 call idea.bat
 cd ..
 
-IF %this_is_idea_update_home% == "" GOTO under_idea_folder_done
+IF "%this_is_idea_update_home%" == "" GOTO under_idea_folder_done
 
   cd ..
   cd idea.update
