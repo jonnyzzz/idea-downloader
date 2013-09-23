a5bdbb819917aeffe2d74b2dddb7f0efff1b8f4c
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-09-23 09:26:35 +0000

    include date and time into the log
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/util/trunk/TeamCity.Downloader@176 5c360b9f-cb11-f648-b5e7-fb5e491e6b3d

--- scheduled-task.bat
+++ scheduled-task.bat
@@ -3,6 +3,7 @@
 echo. >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
 echo Started fetch >> last-fatch-log.txt
+date /T >> last-fatch-log.txt
 time /T >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
 
@@ -11,6 +12,7 @@ cmd /c ant idea-pre-fetch-auto 2>&1 | wtee -a last-fatch-log.txt
 echo. >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
 echo Completed fetch >> last-fatch-log.txt
+date /T >> last-fatch-log.txt
 time /T >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
 echo. >> last-fatch-log.txt
